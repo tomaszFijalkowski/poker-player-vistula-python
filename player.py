ed2c76a12a4e46607e13ee78861df82e6b86e4d0
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-07-23 11:19:12 +0200

    first read gamestate

--- player.py
+++ player.py
@@ -2,11 +2,10 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-<<<<<<< HEAD
-        print(game_state)
-=======
->>>>>>> 479950308fe86c9faea8a857478ea79ce9049a7a
-        return 900
+        print(game_state["current_buy_in"])
+        print(game_state["players"][game_state["in_action"]]["bet"]) 
+        print(game_state["minimum_raise"]))
+        return game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state["minimum_raise"]
 
     def showdown(self, game_state):
         pass
