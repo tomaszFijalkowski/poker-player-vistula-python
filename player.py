2d25402ddddbbce812a37c9106283e9f14014f31
Tomasz Fijalkowski <--global>
2018-07-23 12:12:01 +0200

    print

--- player.py
+++ player.py
@@ -2,26 +2,18 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        logGameState(game_state)
+        return game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state["minimum_raise"]
+
+
+    def logGameState(self, game_state):
         print("_______________________________________________________________________________:")
         print(game_state["current_buy_in"])
         print(game_state["players"][game_state["in_action"]]["bet"]) 
         print(game_state["minimum_raise"])
         print(game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state["minimum_raise"])
-        print("to jest self: ", self.id, " ", self.name)
-        print("to jest test: ", game_state["players"]["name"])
+        print(game_state["players"][game_state["in_action"]]["hole_cards"])
         print("_______________________________________________________________________________XXXXX")
-
-
-        # for i in game_state["players"][self][""]
-        # all_cards = {}
-
-
-
-        return game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state["minimum_raise"]
-
-
-
-
 
 
     def showdown(self, game_state):
