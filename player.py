53de592bf6359dbbac0ff4da9f7e32155840a9f8
Tomasz Fijalkowski <--global>
2018-07-23 11:51:52 +0200

    print

--- player.py
+++ player.py
@@ -1,5 +1,3 @@
-import logic
-
 class Player:
     VERSION = "Default Python folding player"
 
@@ -9,9 +7,20 @@ class Player:
         print(game_state["players"][game_state["in_action"]]["bet"]) 
         print(game_state["minimum_raise"])
         print(game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state["minimum_raise"])
-        logic.join(self, game_state)
+        print("to jest self: ", self)
         print("_______________________________________________________________________________XXXXX")
+
+
+        # for i in game_state["players"][self][""]
+        # all_cards = {}
+
+
+
         return game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state["minimum_raise"]
+
+
+
+
 
 
     def showdown(self, game_state):
