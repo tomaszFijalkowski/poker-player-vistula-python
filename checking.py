7410bf0744e39522fef24dc84d4e79d8c55c8d93
WeronikaDebowska <weronika.debowska@gmail.com>
2018-07-23 14:59:59 +0200

    max and pairs

--- checking.py
+++ checking.py
@@ -1,8 +1,8 @@
 all_cards = [{"rank": "4", "suit": "hearts" },
-{"rank": "K", "suit": "diaonds" },
+{"rank": "6", "suit": "hearts" },
 {"rank": "Q", "suit": "spades" },
-{"rank": "K", "suit": "clubs" },
-{"rank": "A", "suit": "hearts" },
+{"rank": "9", "suit": "clubs" },
+{"rank": "2", "suit": "hearts" },
 {"rank": "A", "suit": "clubs" },
 {"rank": "4", "suit": "spades" },
 ]
@@ -22,3 +22,26 @@ def sort(card_list):
 
     all_cards_sorted = sorted(all_cards, key=lambda k: k['rank'])
     return all_cards_sorted
+
+def find_pair(card_list):
+    pairs = []
+    result = 0
+    for card in card_list:
+        for another_card in card_list:
+            if card['rank'] == another_card['rank']:
+                if card['suit'] != another_card['suit']:
+                    pairs.append(card['rank'])
+                    # result += int(card['rank'])/2
+    print(pairs) 
+    if len(pairs) >4:
+        result = sum(pairs[2:])/2*25
+    else:
+        result = sum(pairs)/2*8
+    return int(result)
+
+def find_max(card_list):
+    result = sort(card_list)[-1]['rank']
+    print(result)
+    return int(result)
+
+find_max(all_cards)
--- player.py
+++ player.py
@@ -39,5 +39,28 @@ class Player:
         all_cards_sorted = sorted(all_cards, key=lambda k: k['rank'])
         return all_cards_sorted
 
+    def find_pair(card_list):
+        pairs = []
+        result = 0
+        for card in card_list:
+            for another_card in card_list:
+                if card['rank'] == another_card['rank']:
+                    if card['suit'] != another_card['suit']:
+                        pairs.append(card['rank'])
+                        # result += int(card['rank'])/2
+        print(pairs) 
+        if len(pairs) >4:
+            result = sum(pairs[2:])/2*25
+        else:
+            result = sum(pairs)/2*8
+        return int(result)
+
+    def find_max(card_list):
+        result = sort(card_list)[-1]['rank']
+        return int(result)
+
+    def find_set(self, card_list):
+        return max(find_max(card_list), find_pair(card_list))
+
     def showdown(self, game_state):
         pass
