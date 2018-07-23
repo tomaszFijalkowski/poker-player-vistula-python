5e2de07910aa80456531e55e02f0f08e8d185f04
Tomasz Fijalkowski <--global>
2018-07-23 10:53:21 +0200

    print

--- player.py
+++ player.py
@@ -2,10 +2,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-<<<<<<< HEAD
         print(game_state)
-=======
->>>>>>> 479950308fe86c9faea8a857478ea79ce9049a7a
         return 900
 
     def showdown(self, game_state):
