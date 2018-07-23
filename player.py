bb9eee193a113281971efe6ec79f72faf8a05549
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-07-23 11:31:41 +0200

    mark logs '__'

--- player.py
+++ player.py
@@ -2,9 +2,11 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        print("_______________________________________________________________________________:")
         print(game_state["current_buy_in"])
         print(game_state["players"][game_state["in_action"]]["bet"]) 
         print(game_state["minimum_raise"])
+        print("_______________________________________________________________________________XXXXX")
         return game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state["minimum_raise"]
 
     def showdown(self, game_state):
