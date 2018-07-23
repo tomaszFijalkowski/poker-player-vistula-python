45b5fe044c0e3ca2e97fe2e168009e3532f0c796
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-07-23 11:22:12 +0200

    amend

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
     def betRequest(self, game_state):
         print(game_state["current_buy_in"])
         print(game_state["players"][game_state["in_action"]]["bet"]) 
-        print(game_state["minimum_raise"]))
+        print(game_state["minimum_raise"])
         return game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state["minimum_raise"]
 
     def showdown(self, game_state):
