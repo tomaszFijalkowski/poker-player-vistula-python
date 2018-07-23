842f42918f0490b5941ff5534872cd5e8d06c8ff
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-07-23 16:55:54 +0200

    attass

--- player.py
+++ player.py
@@ -23,16 +23,16 @@ class Player:
             print("_____________________________________________XXXXXXX:")
             if multiply >= 500:
                 print(multiply)
-                return 900
+                return game_state["players"][game_state["in_action"]]["stack"]
             elif multiply >= 113:
                 print(multiply)
-                return 700
+                return 900
             elif multiply >= 60:
                 print(multiply)
-                return 200
+                return 700
             elif multiply >= 16:
                 print(multiply)
-                return 100
+                return 200
             else:
                 return current_buy_in - my_bet + min_raise
         except Exception as err:
